3d4856f0c3decfb1f49d703acd64b3a6778d976b
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-06-27 16:30:41 +0300

    del

--- setup_mysql_db.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS Healixra;
-CREATE USER IF NOT EXISTS 'Name'@'localhost' IDENTIFIED BY 'Password';
-GRANT ALL PRIVILEGES ON `Healixra`.* TO 'Name'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'Name'@'localhost';
-FLUSH PRIVILEGES;
--- test_flask.py
+++
@@ -1,8 +0,0 @@
-from models.patient import Patient
-from models.base_model import db
-from routes import app
-
-with app.app_context():
-    patient_email = db.session.query(Patient).filter_by(email="elhennawy@ex.com").first()
-    if patient_email:
-        print(patient_email)
