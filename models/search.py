04bade777763f00343993bebf0e91e1db13fe745
Jihan Ahmed <jihanahmed41@gmail.com>
2024-07-01 15:17:12 +0300

    adding search form

--- models/search.py
+++ models/search.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class Search(FlaskForm):
-    specializaion = StringField(
+    specialization = StringField(
         'Specialization',
         validators=[DataRequired()],
         render_kw={'placeholder': 'Specialization'}
@@ -11,6 +11,6 @@ class Search(FlaskForm):
     location = StringField(
         'Location',
         validators=[DataRequired()],
-        render_kw={'placeholder': 'Location'}
+        render_kw={'placeholder': 'Location '}
     )
     submit = SubmitField('Search')
