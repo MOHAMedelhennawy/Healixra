e3b3e333b36e6c5ffd3d5541ff83a92a9ab90f13
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-09-30 07:41:40 +0300

    Fix file

--- setup_project.sh
+++ setup_project.sh
@@ -4,12 +4,12 @@
 set -e
 
 # Prompt user for input
-echo "Please enter username and password:"
-read -p "username: " username
+echo "Please enter name and password:"
+read -p "name: " name
 read -sp "password: " password
 echo ""
 
-if [[ -n $username && -n $password ]]; then
+if [[ -n $name && -n $password ]]; then
     # Check if Python is installed
     if ! command -v python3 &> /dev/null; then
         echo "Python is not installed. Installing Python..."
@@ -30,9 +30,9 @@ if [[ -n $username && -n $password ]]; then
         pip3 install -r requirements.txt
     fi
 
-    # Create database and replace username and password in SQL script
+    # Create database and replace name and password in SQL script
     echo "Setting up the database..."
-    if sed -e "s/{{username}}/$username/g" -e "s/{{password}}/$password/g" setup_mysql_db.sql | sudo mysql -uroot; then
+    if sed -e "s/{{name}}/$name/g" -e "s/{{password}}/$password/g" setup_mysql_db.sql | sudo mysql -uroot; then
         echo "Database setup complete!"
     else
         echo "Database setup failed." && exit 1
@@ -41,18 +41,21 @@ if [[ -n $username && -n $password ]]; then
     # Create fake data
     echo "Creating fake data..."
     for script in create_fake_locations.py create_fake_specializations.py create_fake_doctors.py; do
-        if ! USER=$username PWD=$password HOST=localhost DB=Healixra TYPE_STORAGE=db python3 "$script"; then
+        if ! USER=$name PWD=$password HOST=localhost DB=Healixra TYPE_STORAGE=db python3 "$script"; then
             echo "Failed to execute $script." && exit 1
         fi
     done
 
-    # Start app
-    if USER=$username PWD=$password HOST=localhost DB=Healixra TYPE_STORAGE=db python3 routes.py; then
-        echo "App started successfully!"
-    else
-        echo "Failed to start the app." && exit 1
-    fi
+    # Set env variables
+    export USER=$name
+    export PWD=$password
+    export HOST='localhost'
+    export DB='Healixra'
+    export TYPE_STORAGE='db'
+
+    # Start
+    python3 routes.py
 else
-    echo "Username or password cannot be empty."
+    echo "name or password cannot be empty."
     exit 1
 fi
